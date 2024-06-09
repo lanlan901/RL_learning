69ceb1d9a4cb7b19761332dda9b3c467f8166ecb
lanlan901 <lanlan901@126.com>
2024-06-09 13:43:13 +0200

    04-sarsalambda

---
+++ 03_Sarsa_maze/maze_env.py
@@ -0,0 +1,127 @@
+import numpy as np
+import time
+import sys
+import tkinter as tk
+
+UNIT = 40       # pixels
+MAZE_H = 5      # grid height
+MAZE_W = 5      # grid width
+
+class Maze(tk.Tk, object):
+    def __init__(self):
+        super(Maze, self).__init__()
+        self.action_space = ['u', 'd', 'l', 'r']
+        self.n_actions = len(self.action_space)
+        self.title('maze')
+        self.geometry('{0}x{1}'.format(MAZE_H * UNIT, MAZE_W * UNIT))
+        self._build_maze()
+
+    def _build_maze(self):
+        self.canvas = tk.Canvas(self, bg = 'white',
+                                height= MAZE_H * UNIT,
+                                width= MAZE_W * UNIT)
+
+        # create grids
+        for c in range(0, MAZE_W * UNIT, UNIT):
+            x0, y0, x1, y1 = c, 0, c, MAZE_H * UNIT
+            self.canvas.create_line(x0, y0, x1, y1)
+        for r in range(0, MAZE_H * UNIT, UNIT):
+            x0, y0, x1, y1 = 0, r, MAZE_W * UNIT, r
+            self.canvas.create_line(x0, y0, x1, y1)
+
+        # create origin
+        origin = np.array([20, 20])
+
+        # create obstacles
+        hell1_center = origin + np.array([UNIT * 2, UNIT])      # [100, 60]
+        self.hell1 = self.canvas.create_rectangle(
+            hell1_center[0] - 15, hell1_center[1] - 15,
+            hell1_center[0] + 15, hell1_center[1] + 15,
+            fill='black')
+        hell2_center = origin + np.array([UNIT, UNIT * 2])
+        self.hell2 = self.canvas.create_rectangle(
+            hell2_center[0] - 15, hell2_center[1] - 15,
+            hell2_center[0] + 15, hell2_center[1] + 15,
+            fill='black')
+
+        # create target
+        oval_center = origin + UNIT * 3                         # [140, 140]
+        self.oval = self.canvas.create_oval(
+            oval_center[0] - 15, oval_center[1] - 15,
+            oval_center[0] + 15, oval_center[1] + 15,
+            fill='green')
+
+        # create agent
+        self.rect = self.canvas.create_rectangle(
+            origin[0] - 15, origin[1] - 15,
+            origin[0] + 15, origin[1] + 15,
+            fill='red')
+
+        self.canvas.pack()
+
+    def reset(self):
+        self.update()
+        time.sleep(0.5)
+        self.canvas.delete(self.rect)
+        origin = np.array([20, 20])
+        self.rect = self.canvas.create_rectangle(
+            origin[0] - 15, origin[1] - 15,
+            origin[0] + 15, origin[1] + 15,
+            fill='red')
+        # return current agent coordinate
+        return self.canvas.coords(self.rect)
+
+    # 画布坐标系中，原点在左上角，上移减少，下移增加
+    def step(self, action):
+        s = self.canvas.coords(self.rect)
+        base_action = np.array([0, 0])
+        if action == 0:             # up
+            if s[1] > UNIT:         # if s[1] <= UNIT, agent is at the top
+                base_action[1] -= UNIT
+        elif action == 1:             # down
+            if s[1] < (MAZE_H - 1) * UNIT:
+                base_action[1] += UNIT
+        elif action == 2:             # left
+            if s[0] > UNIT:
+                base_action[0] -= UNIT
+        elif action == 3:             # right
+            if s[0] < (MAZE_W -1) * UNIT:
+                base_action[0] += UNIT
+
+        self.canvas.move(self.rect, base_action[0], base_action[1])
+        s_ = self.canvas.coords(self.rect)      # next state
+
+        # reward function
+        if s_ == self.canvas.coords(self.oval):      # target
+            reward = 1
+            done = True
+            s_ = 'terminal'
+        elif s_ in [self.canvas.coords(self.hell1), self.canvas.coords(self.hell2)]:
+            reward = -1
+            done = True
+            s_ = 'terminal'
+        else:
+            reward = 0
+            done = False
+
+        return s_, reward, done
+
+    def render(self):
+        time.sleep(0.1)
+        self.update()
+
+def update():
+    for t in range(10):
+        s = env.reset()
+        while True:
+            env.render()        # update the environment
+            a = 1
+            s, r, done = env.step(a)
+            if done:
+                break
+
+if __name__ == '__main__':
+    env = Maze()
+    env.after(100, update())
+    env.mainloop()
+
